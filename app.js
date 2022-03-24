fca96941d7f01ab87e6efb3d2d7cabe91551670f
tevsko <tevsko@gmail.com>
2022-03-24 05:58:42 -0300

    Agregado resta

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/',(req, res) => {
-    res.send('Hola a todos');
-});
+app.get('/', function(req, res) {
+    res.send('Hola a todos!');
+  });
 
 app.get('/numen/:alumno', (req, res) =>{
     let alumno = req.params.alumno;
@@ -18,6 +18,19 @@ app.get('/suma/:num1/:num2', (req, res) =>{
     let resultado = num1 + num2;
     res.status(200).json(resultado);
 });
+
+app.get('/resta/:numero1/:numero2', (req, res) =>{
+    let numero1 = Number(req.params.numero1);
+    let numero2 = Number(req.params.numero2);
+    let resultado = numero1 - numero2;
+    console.log(resultado);
+    if (resultado || resultado) {
+        res.status(200).send(`${resultado} es una resta correcta`)
+    } else {
+        res.status(400).send(`${resultado} es una resta incorrecta`)
+    }        
+});
+
 
 app.get('/cosa/:num1',(req, res) =>{
     let num1 = Number (req.params.num1);
